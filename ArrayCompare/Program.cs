951969642a679a096d7da4f8550285389a9d85de
dochi486 <93mskim@gmail.com>
2022-09-04 19:21:04 +0900

    are they "same"?

--- ArrayCompare/Program.cs
+++ ArrayCompare/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ArrayCompare
 {
@@ -6,8 +8,8 @@ namespace ArrayCompare
     {
         static void Main(string[] args)
         {
-            int[] a = new int[] { 121, 144, 19, 161, 19, 144, 19, 10 };
-            int[] b = new int[] { 11 * 11, 121 * 121, 144 * 144, 19 * 19, 161 * 161, 19 * 19, 144 * 144, 19 * 19 };
+            int[] a = new int[] { 121, 144, 19, 161, 19, 144, 19, 11 };
+            int[] b = new int[] { 121, 14641, 20736, 361, 25921, 361, 20736, 361 };
             bool r = AreTheySame.comp(a, b);
             Console.WriteLine(r.ToString());
         }
@@ -16,39 +18,39 @@ namespace ArrayCompare
 
     class AreTheySame
     {
+
         public static bool comp(int[] a, int[] b)
         {
-            bool isContain = false;
-
+           
             if (a == null)
                 return false;
             if (b == null)
                 return false;
 
-            // 배열 a와 b를 비교하는데
-            // b의 요소가 모두 a의 요소들의 제곱값이라면 true
+            if (a.Length == 0)
+                return false;
+            if (b.Length == 0)
+                return false;
 
-            foreach (var item in b)
+            List<int> new_a = a.OrderBy(x => x).ToList();
+
+            for (int i = 0; i < new_a.Count(); i++)
             {
-                for (int i = 0; i < a.Length; i++)
-                {
-                    if (item == Math.Pow(a[i],2))
-                    {
-                        isContain = true;
-                        continue;
-                    }
-                    else
-                    {
-                        isContain = false;
-                        continue;
-                    }
-                       
-                }
+                new_a[i] = new_a[i] * new_a[i];
             }
 
-            // 하나라도 다르면 false
+            List<int> new_b = b.OrderBy(x => x).ToList();
 
-            return isContain;
+            if (new_a.Count() != new_b.Count())
+                return false;
+
+            for (int i = 0; i < new_a.Count(); i++)
+            {
+                if (new_a[i] != new_b[i])
+                    return false;
+            }
+            return true;
+          
         }
     }
 }
